c83670fbc4173514e16592c6e2b697c2737c67c4
Mike Zhou <mike.zhou@gov.bc.ca>
2023-04-26 10:22:27 -0700

    create template main branch

--- demo.sh
+++ demo.sh
@@ -1,17 +1,3 @@
-NAMESPACE=1475a9-test
-TODAY=`date +"%Y-%m-%d"`
+# please enter the job's shell script here
+echo Job1 evoked
 
-echo $TODAY
-echo demo cronjob started
-
-# login open shift
-oc login --token=$OC_TOKEN --server=https://api.silver.devops.gov.bc.ca:6443
-
-# print pods
-for pod in `oc get pods -l app=ols-router-web -n ${NAMESPACE} | grep -v ^NAME | awk '{print $1}'`; do
-    # print out pod name
-    echo $pod;
-done
-
-echo demo cronjob ended
-echo
